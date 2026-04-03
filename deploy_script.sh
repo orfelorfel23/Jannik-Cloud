b834d3dc2ee4072888a38c5647fcdc283867dc2c
orfelorfel23 <jannik.mueller.jannik+git@googlemail.com>
2026-04-03 21:49:50 +0200

    Update cron job to natively sync via hard reset before executing script

--- deploy_script.sh
+++ deploy_script.sh
@@ -194,7 +194,7 @@ setup_swap() {
 ###############################################################################
 setup_cron_job() {
     log "Configuring daily automatic deployment (cron) for 02:31..."
-    local CRON_JOB="31 2 * * * cd /opt/Jannik-Cloud && git pull && /bin/bash /opt/Jannik-Cloud/deploy_script.sh >> /var/log/jannik-cloud-deploy.log 2>&1"
+    local CRON_JOB="31 2 * * * cd /opt/Jannik-Cloud && git fetch origin && git reset --hard origin/main && /bin/bash /opt/Jannik-Cloud/deploy_script.sh >> /var/log/jannik-cloud-deploy.log 2>&1"
     
     # Remove old copies of this cron job to prevent duplicates
     crontab -l 2>/dev/null | grep -v "/opt/Jannik-Cloud/deploy_script.sh" | crontab - || true
